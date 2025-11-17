aa76757b7efe70675e69257df573c342b14c7c3d
KamboJah <sandboxqa11@gmail.com>
2025-11-17 10:45:04 +0200

    add base page common methods

--- Pages/BasePage.cs
+++ Pages/BasePage.cs
@@ -1,6 +1,8 @@
 using AmazonAutomation.Tests.Data;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
 
 namespace AmazonAutomation.Tests.Pages
 {
@@ -12,6 +14,61 @@ namespace AmazonAutomation.Tests.Pages
         public BasePage(IWebDriver driver)
         {
             Driver = driver;
+            Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        protected IWebElement WaitForElement(By locator)
+        {
+            return Wait.Until(ExpectedConditions.ElementIsVisible(locator));
+        }
+
+
+        protected IWebElement WaitForClickable(By locator)
+        {
+            return Wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+        }
+
+
+        protected void Click(By locator)
+        {
+            WaitForClickable(locator).Click();
+        }
+
+
+        protected void Type(By locator, string text)
+        {
+            var element = WaitForElement(locator);
+            element.Clear();
+            element.SendKeys(text);
+        }
+
+
+        protected bool IsElementVisible(By locator)
+        {
+            try
+            {
+                return WaitForElement(locator).Displayed;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        protected void ScrollTo(By locator)
+        {
+            var element = WaitForElement(locator);
+            var js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", element);
+        }
+
+
+        protected void JsClick(By locator)
+        {
+            var element = WaitForClickable(locator);
+            var js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("arguments[0].click();", element);
         }
     }
 }
